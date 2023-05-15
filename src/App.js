8cf340ef9e82d5dbf8aee82876319fc12a1ee393
arpitjaswal <iamarpitjaswal@gmail.com>
2023-05-15 18:43:10 +0530

    basic react structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from "./components/home"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
